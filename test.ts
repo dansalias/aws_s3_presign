152f904b0c65aca2753f79eca5ba20496ac23fc5
Teodor Kostov <tkostov@empowersolutions.eu>
2022-09-01 16:50:48 +0300

    Fix the unit tests

--- test.ts
+++ test.ts
@@ -1,13 +1,10 @@
-import {
-  assertEquals,
-} from 'https://deno.land/std@0.103.0/testing/asserts.ts'
+import { assertEquals } from 'https://deno.land/std@0.103.0/testing/asserts.ts'
 import { getSignedUrl } from './mod.ts'
 
 const date = new Date('Fri, 24 May 2013 00:00:00 GMT')
 
 const baseTestOptions = {
-  bucketName: 'examplebucket',
-  objectPath: '/test.txt',
+  path: '/examplebucket/test.txt',
   region: 'us-east-1',
   accessKeyId: 'AKIAIOSFODNN7EXAMPLE',
   secretAccessKey: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
@@ -18,13 +15,13 @@ Deno.test('creates a presigned URL', () => {
   assertEquals(
     getSignedUrl(baseTestOptions),
     [
-      'https://examplebucket.s3.amazonaws.com/test.txt',
+      'https://s3.amazonaws.com/examplebucket/test.txt',
       '?X-Amz-Algorithm=AWS4-HMAC-SHA256',
       '&X-Amz-Credential=AKIAIOSFODNN7EXAMPLE%2F20130524%2Fus-east-1%2Fs3%2Faws4_request',
       '&X-Amz-Date=20130524T000000Z',
       '&X-Amz-Expires=86400',
       '&X-Amz-SignedHeaders=host',
-      '&X-Amz-Signature=aeeed9bbccd4d02ee5c0109b86d86835f995330da4c265957d157751f604d404',
+      '&X-Amz-Signature=733255ef022bec3f2a8701cd61d4b371f3f28c9f193a1f02279211d48d5193d7',
     ].join('')
   )
 })
@@ -36,14 +33,14 @@ Deno.test('creates a presigned URL with a session token', () => {
       sessionToken: 'AQoEXAMPLEH4aoAH0gNCAPyJxz4BlCFFxWNE1OPTgk5TthT+FvwqnKwRcOIfrRh3c/LTo6UDdyJwOOvEVPvLXCrrrUtdnniCEXAMPLE/IvU1dYUg2RVAJBanLiHb4IgRmpRV3zrkuWJOgQs8IZZaIv2BXIa2R4OlgkBN9bkUDNCJiBeb/AXlzBBko7b15fjrBs2+cTQtpZ3CYWFXG8C5zqx37wnOE49mRl/+OtkIKGO7fAE',
     }),
     [
-      'https://examplebucket.s3.amazonaws.com/test.txt',
+      'https://s3.amazonaws.com/examplebucket/test.txt',
       '?X-Amz-Algorithm=AWS4-HMAC-SHA256',
       '&X-Amz-Credential=AKIAIOSFODNN7EXAMPLE%2F20130524%2Fus-east-1%2Fs3%2Faws4_request',
       '&X-Amz-Date=20130524T000000Z',
       '&X-Amz-Expires=86400',
       '&X-Amz-Security-Token=AQoEXAMPLEH4aoAH0gNCAPyJxz4BlCFFxWNE1OPTgk5TthT%2BFvwqnKwRcOIfrRh3c%2FLTo6UDdyJwOOvEVPvLXCrrrUtdnniCEXAMPLE%2FIvU1dYUg2RVAJBanLiHb4IgRmpRV3zrkuWJOgQs8IZZaIv2BXIa2R4OlgkBN9bkUDNCJiBeb%2FAXlzBBko7b15fjrBs2%2BcTQtpZ3CYWFXG8C5zqx37wnOE49mRl%2F%2BOtkIKGO7fAE',
       '&X-Amz-SignedHeaders=host',
-      '&X-Amz-Signature=3e7fe71f8da45a44c5ee7851dc99611df44ed2171068fa778b3537333ee2b435',
+      '&X-Amz-Signature=77cb31eed6fa73ec3a4bdeab05014a9e387d20e7bea4e132a15159451a73caea',
     ].join('')
   )
 })
